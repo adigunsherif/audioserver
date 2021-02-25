cd1c7ce237964a42d1507ea17dbb1c37ac555be9
Adigun <adigunsherif@gmail.com>
2021-02-25 17:18:59 +0100

    updates

--- test/data.sql
+++ test/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO song (id, name_of_song, duration, uploaded_time)
 VALUES
-  ('1', 'Aluta', 30, '2021-02-25 10:01:33'),
-  ('2', 'Gbamgbam', 20, '2021-02-25 10:01:33');
+  (1, 'Aluta', 30, '2021-02-25 10:01:33'),
+  (2, 'Gbamgbam', 20, '2021-02-25 10:01:33');
 
 INSERT INTO podcast (id, name_of_podcast, duration, uploaded_time, host, participant)
 VALUES
-  (1, 'Chaisom', 240, '2018-01-01 00:00:00', 'Azesmsd', 'sfjkasfd, sfssdfjk, dsfkd');
+  (1, 'Chaisom', 240, '2018-01-01 00:00:00', 'Azesmsd', 'sfjkasfd, sfssdfjk, dsfkd'),
   (2, 'Alabama', 360, '2018-01-01 00:00:00', 'Komaakd', 'Ola walahdsf');
